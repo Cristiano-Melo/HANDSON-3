b62b39b0a37a14bf77f72c330097eaf8c1894fab
amandahammes <82393453+amandahammes@users.noreply.github.com>
2022-07-28 01:01:28 -0300

    dashboard atualizado

--- src/controllers/dashbord.js
+++ src/controllers/dashbord.js
@@ -1,1 +1,28 @@
 const { Atendimentos, Pacientes, Psicologos } = require('../models');
+
+const dashboard = {
+
+    async totalPacientes (req, res){
+        const numeroPacientes = await Pacientes.findAndCountAll();
+        return res.json(`O número total de pacientes é: ${numeroPacientes.count}`);
+    },
+
+    async totalPsicologos (req, res){
+        const { count } = await Psicologos.findAndCountAll();
+        return res.json(`O número total de psicologos é: ${count}`);
+    },
+
+    async totalAtendimentos (req, res){
+        const { count } = await Atendimentos.findAndCountAll();
+        return res.json(`O número total de atendimentos é: ${count}`);
+    },
+
+    async mediaTotal (req, res){
+        const numPiscologos = await Psicologos.findAndCountAll();
+        const numAtendimentos = await Atendimentos.findAndCountAll();
+        return res.json(`A média de att para psico é: ${numAtendimentos.count / numPiscologos.count}`);
+    },
+    
+};
+
+module.exports = dashboard;
